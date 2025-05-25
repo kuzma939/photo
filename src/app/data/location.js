d9841be54294e7f9e2c09d3129ccb9cd3534deaa
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-25 15:22:39 +0200

    Create location.js

---
+++ src/app/data/location.js
@@ -0,0 +1,47 @@
+const location = [
+
+    {
+      id: 117,
+      price: 3100,
+      isTop: true,
+      sku: "67216",
+      size: "XS S M L XL",
+      category: "Dresses",
+      location: "Parc de Montjuïc",
+      type: "Famille", 
+      image: "/Dress/Elodie Dress/1.avif",
+      images: [
+        "/Dress/Elodie Dress/2.avif",
+        "/Dress/Elodie Dress/3.avif",
+        "/Dress/Elodie Dress/4.avif",
+        "/Dress/Elodie Dress/5.avif"
+      ],
+      sizes: ["XS", "S", "M", "L", "XL"],
+      translations: {
+        EN: {
+          category: "Dresses",
+          name: "Elodie Dress",
+          description: "A dress crafted from long-fiber muslin.\nFabric: 100% cotton muslin, light and soft to the touch.",
+          colors: ["powder"]
+        },
+        FR: {
+          category: "Robes",
+          name: "Robe Elodie",
+          description: "Une robe confectionnée en mousseline à longues fibres.\nTissu : mousseline 100 % coton, légère et douce au toucher.",
+          colors: ["poudre"]
+        },
+        UA: {
+          category: "Сукні",
+          name: "Сукня 'Elodie'",
+          description: "Сукня з довговолокняного мусліну.\nТканина: довговолокняний муслін зі 100% бавовни. Легка та мʼяка тканина на дотик.",
+          colors: ["пудра"]
+        },
+        RU: {
+          category: "Платья",
+          name: "Платье 'Элоди'",
+          description: "Платье из длинноволокнистого муслина.\nТкань: муслин из 100% хлопка, легкая и мягкая на ощупь.",
+          colors: ["пудра"]
+        }
+      }
+    },
+]

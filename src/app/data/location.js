7f0ef6ca7f55eca2321524b5e9483dbd14dda377
yahyafullstack01 <yahyafullstack01@gmail.com>
2025-07-26 11:41:42 +0200

    add

--- src/app/data/location.js
+++ src/app/data/location.js
@@ -4,10 +4,10 @@ const location = [
     location: "Gothic Quarter",
     banner: "/Gothic/TURKEY/1.avif",
     type: "Couple",
-    images: ["/6.avif","/Gothic/2.avif","/Gothic/JAPAN/3.avif", "/Gothic/KIM AND KERRY/2.avif", "/Gothic/OLD USA/1.avif", "/Gothic/USA ITA/13.avif" , "/Gothic/Moldova/2.avif", "/Gothic/CHINESS/13.avif", "/Gothic/USA GIRL/1.avif", "/Gothic/PHILIPIN FAMILY USA/3.avif", 
-      "/Gothic/JAPAN/2.avif", "/Gothic/Moldova/6.avif", "/Gothic/OLD USA/5.avif", "/Gothic/CHINESS/16.avif", "/Gothic/USA ITA/11.avif", "/Gothic/PHILIPIN FAMILY USA/5.avif", "/Gothic/USA GIRL/5.avif",  "/Gothic/JAPAN/4.avif", "/Gothic/KIM AND KERRY/5.avif", "/Gothic/philipin/35.avif", "/Gothic/USA ITA/8.avif" , "/Gothic/OLD USA/6.avif", 
-      "/Gothic/JAPAN/8.avif", "/Gothic/Moldova/9.avif", "/Gothic/PHILIPIN FAMILY USA/7.avif", "/Gothic/CHINESS/17.avif", "/Gothic/USA ITA/25.avif", "/Gothic/JAPAN/9.avif", "/Gothic/KIM AND KERRY/1.avif", "/Gothic/philipin/44.avif", "/Gothic/Moldova/13.avif", "/Gothic/CHINESS/18.avif", "/Gothic/USA ITA/26.avif" , "/Gothic/KIM AND KERRY/18.avif", "/Gothic/KIM AND KERRY/22.avif",
-      "/Gothic/KIM AND KERRY/28.avif", "/Gothic/Moldova/20.avif", "/Gothic/USA GIRL/6.avif", "/Gothic/KIM AND KERRY/48.avif", "/Gothic/USA ITA/40.avif", "/Gothic/KIM AND KERRY/33.avif" , "/Gothic/Moldova/22.avif", "/Gothic/USA GIRL/7.avif", "/Gothic/KIM AND KERRY/56.avif", "/Gothic/philipin/40.avif",
+    images: ["/6.avif","/Gothic/2.avif","/Gothic/JAPAN/3.avif", "/Gothic/KIM AND KERRY/2.avif", "/Gothic/TURKEY/2.avif", "/Gothic/OLD USA/1.avif", "/Gothic/USA ITA/13.avif" , "/Gothic/Moldova/2.avif", "/Gothic/TURKEY/4.avif", "/Gothic/CHINESS/13.avif", "/Gothic/USA GIRL/1.avif", "/Gothic/PHILIPIN FAMILY USA/3.avif", 
+      "/Gothic/JAPAN/2.avif", "/Gothic/Moldova/6.avif", "/Gothic/TURKEY/1.avif", "/Gothic/OLD USA/5.avif", "/Gothic/CHINESS/16.avif", "/Gothic/USA ITA/11.avif", "/Gothic/PHILIPIN FAMILY USA/5.avif", "/Gothic/USA GIRL/5.avif",  "/Gothic/JAPAN/4.avif", "/Gothic/KIM AND KERRY/5.avif", "/Gothic/TURKEY/2.avif", "/Gothic/OLD USA/1.avif", "/Gothic/USA ITA/13.avif" , "/Gothic/Moldova/2.avif", "/Gothic/TURKEY/18.avif", "/Gothic/philipin/35.avif", "/Gothic/USA ITA/8.avif" , "/Gothic/OLD USA/6.avif", 
+      "/Gothic/JAPAN/8.avif", "/Gothic/Moldova/9.avif", "/Gothic/PHILIPIN FAMILY USA/7.avif", "/Gothic/CHINESS/17.avif", "/Gothic/USA ITA/25.avif", "/Gothic/TURKEY/2.avif", "/Gothic/OLD USA/1.avif", "/Gothic/USA ITA/13.avif" , "/Gothic/Moldova/2.avif", "/Gothic/TURKEY/12.avif", "/Gothic/JAPAN/9.avif", "/Gothic/KIM AND KERRY/1.avif", "/Gothic/philipin/44.avif", "/Gothic/Moldova/13.avif", "/Gothic/CHINESS/18.avif", "/Gothic/USA ITA/26.avif" , "/Gothic/KIM AND KERRY/18.avif", "/Gothic/KIM AND KERRY/22.avif",
+      "/Gothic/KIM AND KERRY/28.avif", "/Gothic/Moldova/20.avif", "/Gothic/USA GIRL/6.avif", "/Gothic/KIM AND KERRY/48.avif", "/Gothic/USA ITA/40.avif", "/Gothic/KIM AND KERRY/33.avif" , "/Gothic/Moldova/22.avif", "/Gothic/USA GIRL/7.avif", "/Gothic/KIM AND KERRY/56.avif", "/Gothic/philipin/40.avif", "/Gothic/TURKEY/28.avif",
       "/Gothic/KIM AND KERRY/53.avif", "/Gothic/Moldova/39.avif", "/Gothic/KIM AND KERRY/54.avif", "/Gothic/Moldova/44.avif", "/Gothic/PHILIPIN FAMILY USA/15.avif", "/Gothic/Moldova/46.avif", "/Gothic/philipin/31.avif", "/Gothic/USA GIRL/8.avif", "/Gothic/Moldova/50.avif", "/Gothic/PHILIPIN FAMILY USA/8.avif", "/Gothic/Moldova/52.avif", "/Gothic/PHILIPIN FAMILY USA/9.avif", "/Gothic/philipin/38.avif"],
     translations: {
       EN: { name: "Gothic Quarter" },
@@ -17,7 +17,7 @@ const location = [
   },
 
     {
-    id: 4,
+    id: 2,
     location: "Ciutadella Park",
     banner: "/Parc cuitadella/NATASHA/5.avif",
     type: "Couple",
@@ -39,9 +39,9 @@ const location = [
     {
     id: 3,
     location: "Sagrada Família",
-    banner: "/Parc cuitadella/NATASHA/5.avif",
+    banner: "/3.avif",
     type: "Couple",
-    images: ["/Parc cuitadella/PIE AND JARON/21.avif","/Parc cuitadella/NATASHA/3.avif","/Parc cuitadella/PIE AND JARON/6.avif"
+    images: ["/3.avif","/Parc cuitadella/NATASHA/3.avif","/Parc cuitadella/PIE AND JARON/6.avif"
     ],
     translations: {
       EN: { name: "Sagrada Família" },
@@ -62,6 +62,24 @@ const location = [
   //     UA: { name: "Собор Барселони" }
   //   }
   // },
+
+    {
+    id: 5,
+    location: "Manjuic",
+    banner: "/banners/cathedral.jpg",
+    type: "Couple",
+    images: ["/Manjuic/1.avif", "/Manjuic/2.avif","/Manjuic/4.avif", "/Manjuic/6.avif","/Manjuic/3.avif",
+      "/Manjuic/8.avif","/Manjuic/10.avif","/Manjuic/14.avif", "/Manjuic/17.avif","/Manjuic/23.avif",
+      "/Manjuic/27.avif",
+
+    ],
+    translations: {
+      EN: { name: "Manjuic" },
+      FR: { name: "" },
+      UA: { name: "Manjuic" }
+    }
+  },
+  
   // {
   //   id: 3,
   //   location: "Sagrada Família",
